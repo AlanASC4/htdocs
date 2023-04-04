b3f202a1bb69e124c5bb04f0035829d6419d89d5
AlanASC4 <alan.torres0229@gmail.com>
2023-04-03 23:28:34 -0400

    tbl

--- databaseDemo/createTBL.php
+++ databaseDemo/createTBL.php
@@ -10,16 +10,40 @@ if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
 // sql to create table
-$sql = "CREATE TABLE myStudents (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(20) NOT NULL,
-lastname VARCHAR(20) NOT NULL,
-email VARCHAR(40))";
+$sql = "CREATE TABLE tblproduct (
+id INT(8) NOT NULL,
+name VARCHAR(255) NOT NULL,
+code VARCHAR(255) NOT NULL,
+image text NOT NULL,
+price double(10,2) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = latin1";
 if ($conn->query($sql) === TRUE) {
-echo "Table myStudents created successfully";
+echo "Table tblproduct created successfully";
 } else {
 echo "Error creating table: " . $conn->error;
 }
+
+$sql = "INSERT INTO tblproduct (id, name, code, image, price)
+VALUES (1, 'FinePix Pro2 3D Camera', '3DcAM01', 'product-images/camera.jpg', 1500.00);";
+$sql = "INSERT INTO tblproduct (id, name, code, image, price)
+VALUES (2, 'EXP Portable Hard Drive', 'USB02', 'product-images/external-hard-drive.jpg', 800.00);";
+$sql = "INSERT INTO tblproduct (id, name, code, image, price)
+VALUES (3, 'Luxury Ultra thin Wrist Watch', 'wristWear03', 'product-images/watch.jpg', 300.00);";
+$sql = "INSERT INTO tblproduct (id, name, code, image, price)
+VALUES (4, 'XP 1155 Intel Core Laptop', 'LPN45', 'product-images/laptop.jpg', 800.00);";
+
+if (mysqli_multi_query($conn, $sql)) {
+echo "New records created successfully";
+} else { echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+$sql = "ALTER TABLE tblproduct
+ADD PRIMARY KEY('id');
+ADD UNIQUE KEY 'product_code' ('code')";
+
+$sql = "ALTER TABLE tblproduct
+MODIFY id int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT";
 
 $conn->close();
 ?>
--- databaseDemo/fillTBL.php
+++ databaseDemo/fillTBL.php
@@ -1,9 +1,10 @@
 <?php
 include("connect.php");
-$sql = "INSERT INTO myStudents (id, firstname, lastname, email)
-VALUES (123, 'John', 'Doe', 'jd123@phpworld.com');";
-$sql .= "INSERT INTO myStudents (id, firstname, lastname, email)
-VALUES (345, 'Jane', 'Iyer', 'ji345@phpworld.com');";
+
+$sql = "INSERT INTO tblproduct (id, name, code, image, price)
+VALUES (1, 'FinePix Pro2 3D Camera', '3DcAM01', 'product-images/camera.jpg', 1500.00);";
+
+
 if (mysqli_multi_query($conn, $sql)) {
 echo "New records created successfully";
 } else { echo "Error: " . $sql . "<br>" . mysqli_error($conn);
